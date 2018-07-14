7add942cde6b36ee47d0e186aa67a60943792313
Le Tri Thai <thai1225@gmail.com>
2018-07-14 16:12:40 +0700

    basic snmp walk

---
+++ src/main/java/com/egs/snmp/SnmpWalk.java
@@ -0,0 +1,80 @@
+package com.egs.snmp;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.snmp4j.CommunityTarget;
+import org.snmp4j.Snmp;
+import org.snmp4j.Target;
+import org.snmp4j.TransportMapping;
+import org.snmp4j.mp.SnmpConstants;
+import org.snmp4j.smi.Address;
+import org.snmp4j.smi.GenericAddress;
+import org.snmp4j.smi.OID;
+import org.snmp4j.smi.OctetString;
+import org.snmp4j.smi.VariableBinding;
+import org.snmp4j.transport.DefaultUdpTransportMapping;
+import org.snmp4j.util.DefaultPDUFactory;
+import org.snmp4j.util.TreeEvent;
+import org.snmp4j.util.TreeUtils;
+
+public class SnmpWalk {
+
+    public static void main(String[] args) throws Exception {
+        CommunityTarget target = new CommunityTarget();
+        target.setCommunity(new OctetString("secreteCommunityString"));
+        target.setAddress(GenericAddress.parse("udp:agent-ip-address/161")); // supply your own IP and port
+        target.setRetries(2);
+        target.setTimeout(1500);
+        target.setVersion(SnmpConstants.version2c);
+
+        Map<String, String> result = doWalk(".1.3.6.1.2.1", target); // ifTable, mib-2 interfaces
+
+        for (Map.Entry<String, String> entry : result.entrySet()) {
+            System.out.println(String.format("key: %s, value: %s", entry.getKey(), entry.getValue()));
+        }
+    }
+
+    public static Map<String, String> doWalk(String tableOid, Target target) throws IOException {
+        Map<String, String> result = new TreeMap<>();
+        TransportMapping<? extends Address> transport = new DefaultUdpTransportMapping();
+        Snmp snmp = new Snmp(transport);
+        transport.listen();
+
+        TreeUtils treeUtils = new TreeUtils(snmp, new DefaultPDUFactory());
+        List<TreeEvent> events = treeUtils.getSubtree(target, new OID(tableOid));
+        if (events == null || events.size() == 0) {
+            System.out.println("Error: Unable to read table...");
+            return result;
+        }
+
+        for (TreeEvent event : events) {
+            if (event == null) {
+                continue;
+            }
+            if (event.isError()) {
+                System.out.println("Error: table OID [" + tableOid + "] " + event.getErrorMessage());
+                continue;
+            }
+
+            VariableBinding[] varBindings = event.getVariableBindings();
+            if (varBindings == null || varBindings.length == 0) {
+                continue;
+            }
+            for (VariableBinding varBinding : varBindings) {
+                if (varBinding == null) {
+                    continue;
+                }
+
+                result.put("." + varBinding.getOid().toString(), varBinding.getVariable().toString());
+            }
+
+        }
+        snmp.close();
+
+        return result;
+    }
+
+}
